4a26cde7fdf189fcc9c574f6ba694ebf7979f693
Pramudya Diva Alamsyah <pramudyaalamsyah@gmail.com>
2023-01-29 17:18:26 +0700

    package

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/PramudyaDivaAlamsyah/GO_SAY_HELLO.git //nama modulenya
+
+go 1.18 //versi go nya
---
+++ say-hello.go
@@ -0,0 +1,5 @@
+package gosayhello //cara membuat module di go lang
+
+func SayHello(){//di go lang harus huruf besar duluan untuk membuat function
+	return "hello"
+}
