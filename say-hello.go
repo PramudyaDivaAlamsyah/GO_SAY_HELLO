d20da2a146a0325e4d6cee4aca9c68ff2a5ce7df
Pramudya Diva Alamsyah <pramudyaalamsyah@gmail.com>
2023-02-11 16:08:48 +0700

    update

--- say-hello.go
+++ say-hello.go
@@ -1,13 +1,15 @@
 package gosayhello //cara membuat module di go lang
 
-func SayHello() string{//di go lang harus huruf besar duluan untuk membuat function
-	return "hello";
+func SayHello() string { //di go lang harus huruf besar duluan untuk membuat function
+	return "hello"
 }
 
 //go module harus membuat versi denga huruf depan v contoh v1.0.0
-//buat di git tag untuk membuat modulenya... 
+//buat di git tag untuk membuat modulenya...
 
 //contoh membuat relase ke git
-//contoh: 
+//contoh:
 //1. git tag v1.0.0
 //2. git push origin v1.0.0
+
+//cara update
